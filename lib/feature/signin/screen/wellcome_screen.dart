f3d8f29b50a4dc321131b949d701dad0bb5bb9c5
Zain <muhammadzainidrees@gmail.com>
2026-04-29 12:14:42 +0500

    well come screen

--- lib/feature/signin/screen/wellcome_screen.dart
+++ lib/feature/signin/screen/wellcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clarity/widgets/custom_container.dart';
 import 'package:flutter/material.dart';
 
 class WellcomeScreen extends StatefulWidget {
@@ -10,6 +11,44 @@ class WellcomeScreen extends StatefulWidget {
 class _WellcomeScreenState extends State<WellcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Color(0xffFAF8FF),body: ,);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffffffff),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Clarity',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight(800),
+                color: Color(0xff2563EB),
+              ),
+            ),
+            Text(
+              'Skip',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight(600),
+                color: Color(0xff2563EB),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color(0xffFAF8FF),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(25),
+        child: Column(
+          children: [
+            CustomContainer(
+              child: Image.asset('assets/Mouse.png'),
+              height: 350,
+              width: 350,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
